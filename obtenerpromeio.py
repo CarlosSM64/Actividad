fc80083eac7c0f9320b0e74d23aaeff528c2c3bf
Carlos <carlos.mantilla@tecnologicoliceocristiano.edu.ec>
2024-07-11 23:49:20 -0500

    message

---
+++ obtenerpromeio.py
@@ -0,0 +1,28 @@
+# ok, elaborar un algoritmo que solicite 3 números
+# y que aquellos muestren promedio de todos
+
+# Se inicia con input
+# y defina dos varibales
+# Además de agrega int
+
+N1 = input("Ingresar un número 1: ")
+
+Num1 = int(N1)
+
+N2 = input("Ingresar un número 2: ")
+
+Num2 = int(N2)
+
+N3 = input("Ingresa un número 3: ")
+
+Num3 = int(N3)
+
+# Y después el promedio, definiendo S y P
+
+S = Num1 + Num2 + Num3
+
+print(S)
+
+P = S/3
+
+print(P)
